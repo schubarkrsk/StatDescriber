9a4362aa28e07d9733365233014b1d99278af688
Stanislav <stas-chubar@yandex.ru>
2023-04-03 17:28:52 +0700

    start pypi lib structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='statdescriber',
+    version='1.0.0',
+    packages=['statdescriber'],
+    url='https://github.com/schubarkrsk/StatDescriber',
+    license='GNU GPL V3',
+    author='Stanislav Chubar',
+    author_email='stas-chubar@yandex.ru',
+    description='This is small pakage for my university course project. It help calculate statistic for massive of csv files'
+)
